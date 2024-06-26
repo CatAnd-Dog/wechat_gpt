3d777d0ffa607826a37e40b669c0ba17d06e0d87
yijia <13262866275@163.com>
2024-06-26 16:47:20 +0800

    服务号

--- wechat/__init__.py
+++ wechat/__init__.py
@@ -23,19 +23,14 @@ class clt():
         
 
     def send_text(self,user,msg):
-        # tag=self.func.usertag(user)['tagid_list']  # 权限控制部分
+        tag=self.func.usertag(user)['tagid_list']  # 权限控制部分
         reply="你好，请联系作者"
-        if msg.startswith('gpt'):
+        if 101 in tag and msg.startswith('gpt'):
             parts  = msg.split(' ', 1)
             reply=self.chat_msg.chat_gpt(parts[1],parts[0])
         else:
             reply=self.chat_msg.chat_gpt(msg,defalut_model)
-        # msg以gpt开头
-        # if 100 in tag and msg.startswith('gpt4'):
-        #     reply=self.getreply(msg,'chat-gpt-4')
-        # elif 101 in tag and msg.startswith('gpt3'):
-        #     reply=self.getreply(msg,'gpt-3.5-turbo-1106')
-        
+
         # 如果回复长度超过 500 字符，分批发送
         for start in range(0, len(reply), 500):
             # 截取从 start 到 start+500 的字符，发送
@@ -44,11 +39,11 @@ class clt():
 
     def send_muban(self, template_id,user, urlred,content):
         self.muban.sendmuban(template_id, user, urlred, content)
-        # tag=self.func.usertag(user)['tagid_list']
-        # if 109 in tag:
-        #     self.muban.sendmuban(template_id, user, urlred, content)
-        #     return '发送成功'
-        # return '无权限'
+        tag=self.func.usertag(user)['tagid_list']
+        if 109 in tag:
+            self.muban.sendmuban(template_id, user, urlred, content)
+            return '发送成功'
+        return '无权限'
 
 class dbdata_clt():
     def __init__(self) :
